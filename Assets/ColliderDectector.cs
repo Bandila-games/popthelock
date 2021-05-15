f445b91b48d22487ac2c22f0027bb8b2242ca769
Angelo <angelo@wehi.cc>
2021-05-15 23:45:49 +0800

    add initial project development

---
+++ Assets/ColliderDectector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ColliderDectector : MonoBehaviour
+{
+
+    [SerializeField] Image test;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        test.color = Color.green;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        test.color = Color.green;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        test.color = Color.white;
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] RectTransform ControllerCircle;
+    [SerializeField] RectTransform TargetCircle;
+    [SerializeField] float rotationSpeed = 1;
+
+    int currentLevel = 1;
+
+    bool isClockWise = false;
+    bool isGameStart = false;
+
+    private void Awake()
+    {
+        StartGame();
+    }
+
+
+    public void StartGame()
+    {
+        //RRotate
+        isGameStart = true;
+        StartCoroutine(RotateControllerCircle());
+    }
+
+    public IEnumerator RotateControllerCircle()
+    {
+
+        while(isGameStart)
+        {
+            if(isClockWise)
+            {
+                //rotate clock wise
+                LeanTween.rotateZ(ControllerCircle.gameObject, ControllerCircle.eulerAngles.z + rotationSpeed, 0.01f);
+              
+            }
+            else
+            {
+                //rotate counter clockwise
+                LeanTween.rotateZ(ControllerCircle.gameObject, ControllerCircle.eulerAngles.z - rotationSpeed, 0.01f);
+            }
+            yield return null;
+        }
+
+
+        yield return null;
+    }
+
+
+    public void CounterRotation()
+    {
+        isClockWise = !isClockWise;
+        rotationSpeed += 0.25f;
+        LeanTween.rotateZ(TargetCircle.gameObject, Random.Range(0,360), 0.01f);
+
+    }
+
+
+}
