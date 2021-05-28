57c58ce8e5c11c24bdf3cc5405537daa30bd1fe9
Angelo <angelo@wehi.cc>
2021-05-28 15:31:17 +0800

    add ads module

--- Assets/Modules/Gameplay/ColliderDectector.cs
+++ Assets/Modules/Gameplay/ColliderDectector.cs
@@ -9,19 +9,31 @@ public class ColliderDectector : MonoBehaviour
 
     [SerializeField] Image test;
 
+    public bool IsColliding;
+
+    private bool _IsCollding;
+    private Color OriginalColor;
+    private void Awake()
+    {
+        OriginalColor = test.color;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         test.color = Color.green;
+        IsColliding = true;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
 
         test.color = Color.green;
+        IsColliding = true;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        test.color = Color.white;
+        test.color = OriginalColor;
+        IsColliding = false;
     }
 }
